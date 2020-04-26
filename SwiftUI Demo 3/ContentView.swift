e79f46f00765780dffb1e51cf03943d617a45b10
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-04-25 22:29:48 -0700

    first commit

---
+++ SwiftUI Demo 3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Demo 3
+//
+//  Created by Sebastian Abarca on 4/25/20.
+//  Copyright © 2020 Sebastian Abarca. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Modify Me")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
