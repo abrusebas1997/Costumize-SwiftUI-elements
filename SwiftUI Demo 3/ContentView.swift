8eda9182289bbfdd86458e788f880b46ecaff971
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-04-27 14:51:24 -0700

    [finished] stretch challenge

--- SwiftUI Demo 3/ContentView.swift
+++ SwiftUI Demo 3/ContentView.swift
@@ -10,12 +10,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("asdassadasdsadsadasdasdasdasdasdasdasdsadadsadasdasd")
-            .fontWeight(.heavy)
-            .foregroundColor(Color.purple)
+   
+            ZStack {
+                Image("Breakfast")
+                    .resizable()
+                    .cornerRadius(30)
+                    .edgesIgnoringSafeArea(.all)
+
                 
-                .multilineTextAlignment(.center)
-            .padding(.horizontal, 50.0)
+                
+                Image("TopGradient")
+                    .position(.init(x: 207, y: 140))
+                Spacer()
+                Text("Hardcore")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .shadow(color: .white, radius: 0.5, x: 0.3, y: 0.3)
+                    .position(.init(x: 77, y: 60))
+                Text("Avo Toast")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .shadow(color: .white, radius: 0.5, x: 0.3, y: 0.3)
+                    .position(.init(x: 77, y: 100))
+                    
+                Image("BottomGradient")
+                    .position(.init(x: 207, y: 750))
+                Image("Heart_health")
+                .position(.init(x: 350, y: 60))
+                .shadow(color: .white, radius: 5)
+                Spacer()
+                Text("Energize with this healthy and hearty breakfast")
+                
+                .fontWeight(.medium)
+                .foregroundColor(.white)
+                    .shadow(color: .white, radius: 0.5, x: 0.3, y: 0.3)
+                    .position(.init(x: 207, y: 800))
+            
+        }
     }
 }
 
